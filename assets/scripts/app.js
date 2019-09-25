706f16eb8b9db983d62501b078c858c50c3eb0dc
sangelici <sangelici94@gmail.com>
2019-09-25 10:40:16 -0400

    Adding directories and files to run grunt deploy

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example')
+const events = require ('./game/events.js')
+// use require without a reference to ensure a file is bundled
+// require('./example')
+
+$(() => {
+  $('#gameboard').on('click', events.onPlayerMove)
+})
