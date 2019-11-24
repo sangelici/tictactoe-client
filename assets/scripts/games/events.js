3cf8d0a5db225167bb6fe99efde7009431531b04
sangelici <sangelici94@gmail.com>
2019-11-24 15:17:43 -0500

    Reformat game 'event.js'
    - remove linter warnings from file

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -2,39 +2,42 @@
 
 const api = require('./api.js')
 const ui = require('./ui.js')
-const store = require ('../store.js')
+const store = require('../store.js')
 
-//______________________________________GLOBALS
-const gameboard = ["","","","","","","","",""]
+// ______________________________________GLOBALS
+const gameboard = ['', '', '', '', '', '', '', '', '']
 let playerTurn = true
 let draw = false
 let winner = false
 let gameover = false
 
-const onCreateGame = function(event) {
+const onCreateGame = function (event) {
   event.preventDefault()
   reset()
   api.createGame()
     .then(ui.onCreateGameSuccess)
 }
-const onUpdateGame = function(event) {
+
+const onUpdateGame = function (event) {
   event.preventDefault()
   store.id = $(event.target).attr('data-cell-index')
 }
-const onShowGames = function() {
-    event.preventDefault()
-    api.showGames()
-      .then(ui.onShowGamesSuccess)
-      .catch(ui.onShowGamesFailure)
-}
-//______________________________________GAME LOGIC
-const onBoxClicked = function(event) {
+
+const onShowGames = function () {
   event.preventDefault()
-  let letter = playerTurn ? 'X' : 'O'
+  api.showGames()
+    .then(ui.onShowGamesSuccess)
+    .catch(ui.onShowGamesFailure)
+}
+
+// ______________________________________GAME LOGIC
+const onBoxClicked = function (event) {
+  event.preventDefault()
+  const letter = playerTurn ? 'X' : 'O'
   console.log(letter)
-  if ($(event.target).html() === ""){
+  if ($(event.target).html() === '') {
     $(event.target).html(letter).css('color', '#FF8E51').css('font-family', 'Cute Font')
-    if(playerTurn) {
+    if (playerTurn) {
       playerTurn = false
       $('#game-message').text(`O's Turn!`).css('color', 'blue')
     } else {
@@ -55,13 +58,13 @@ const onBoxClicked = function(event) {
     $('#tile-taken').text('Tile taken!').show()
   }
 }
-const winStyling = function() {
+const winStyling = function () {
   $('#game-message').html(`Winner!`).css('color', '#3DFF4D').css('background-color', '#ffffff')
   $('.box').off('click', onBoxClicked)
   $('#create-game').text('Play Again?')
   // use variables or an if statement to create win highlight to DRY code
 }
-const checkForDraw = function() {
+const checkForDraw = function () {
   if ($('#0').html() !== '' && $('#1').html() !== '' && $('#2').html() !== '' && $('#3').html() !== '' && $('#4').html() !== '' && $('#5').html() !== '' && $('#6').html() !== '' && $('#7').html() !== '' && $('#8').html() !== '') {
     draw = true
     $('#0').css('border-color', '#EE6352')
@@ -78,54 +81,54 @@ const checkForDraw = function() {
     $('#create-game').text('Play Again?')
   }
 }
-const checkForWin = function() {
+const checkForWin = function () {
   // HORIZONTAL
-  if($('#0').html() !== '' && $('#0').html() === $('#1').html() && $('#0').html() ===  $('#2').html()) {
+  if ($('#0').html() !== '' && $('#0').html() === $('#1').html() && $('#0').html() ===  $('#2').html()) {
     winner = true
     winStyling()
     $('#0').css('border-color', '#3DFF4D')
     $('#1').css('border-color', '#3DFF4D')
     $('#2').css('border-color', '#3DFF4D')
-  } else if($('#3').html() !== '' && $('#3').html() === $('#4').html() && $('#3').html() ===  $('#5').html()){
+  } else if ($('#3').html() !== '' && $('#3').html() === $('#4').html() && $('#3').html() === $('#5').html()) {
     winner = true
     winStyling()
     $('#3').css('border-color', '#3DFF4D')
     $('#4').css('border-color', '#3DFF4D')
     $('#5').css('border-color', '#3DFF4D')
-  } else if($('#6').html() !== '' && $('#6').html() === $('#7').html() && $('#6').html() ===  $('#8').html()) {
+  } else if ($('#6').html() !== '' && $('#6').html() === $('#7').html() && $('#6').html() === $('#8').html()) {
     winner = true
     winStyling()
     $('#6').css('border-color', '#3DFF4D')
     $('#7').css('border-color', '#3DFF4D')
     $('#8').css('border-color', '#3DFF4D')
-  //VERTICAL
-} else if($('#0').html() !== '' && $('#0').html() === $('#3').html() && $('#0').html() ===  $('#6').html()) {
+  // VERTICAL
+  } else if ($('#0').html() !== '' && $('#0').html() === $('#3').html() && $('#0').html() === $('#6').html()) {
     winner = true
     winStyling()
     $('#0').css('border-color', '#3DFF4D')
     $('#3').css('border-color', '#3DFF4D')
     $('#6').css('border-color', '#3DFF4D')
-  } else if($('#1').html() !== '' && $('#1').html() === $('#4').html() && $('#1').html() ===  $('#7').html()) {
+  } else if ($('#1').html() !== '' && $('#1').html() === $('#4').html() && $('#1').html() === $('#7').html()) {
     winner = true
     winStyling()
     $('#1').css('border-color', '#3DFF4D')
     $('#4').css('border-color', '#3DFF4D')
     $('#7').css('border-color', '#3DFF4D')
-  } else if($('#2').html() !== '' && $('#2').html() === $('#5').html() && $('#2').html() ===  $('#8').html()) {
-    //console.log('Winner!')
+  } else if ($('#2').html() !== '' && $('#2').html() === $('#5').html() && $('#2').html() === $('#8').html()) {
+    // console.log('Winner!')
     winner = true
     winStyling()
     $('#2').css('border-color', '#3DFF4D')
     $('#5').css('border-color', '#3DFF4D')
     $('#8').css('border-color', '#3DFF4D')
-  // // DIAGONAL
-} else if($('#0').html() !== '' && $('#0').html() === $('#4').html() && $('#0').html() ===  $('#8').html()) {
+  // DIAGONAL
+  } else if ($('#0').html() !== '' && $('#0').html() === $('#4').html() && $('#0').html() === $('#8').html()) {
     winner = true
     winStyling()
     $('#0').css('border-color', '#3DFF4D')
     $('#4').css('border-color', '#3DFF4D')
     $('#8').css('border-color', '#3DFF4D')
-  } else if($('#2').html() !== '' && $('#2').html() === $('#4').html() && $('#2').html() ===  $('#6').html()) {
+  } else if ($('#2').html() !== '' && $('#2').html() === $('#4').html() && $('#2').html() === $('#6').html()) {
     winner = true
     winStyling()
     $('#2').css('border-color', '#3DFF4D')
@@ -133,7 +136,8 @@ const checkForWin = function() {
     $('#6').css('border-color', '#3DFF4D')
   }
 }
-const reset = function() {
+
+const reset = function () {
   playerTurn = true
   winner = false
   $('#game-message').text('')
@@ -142,7 +146,7 @@ const reset = function() {
   $('.box').css('border-color', '#728CFF')
   $('.box').on('click', onBoxClicked)
 }
-//______________________________________MUSIC
+// ______________________________________MUSIC
 // const onPlayMusic = function() {
 //   console.log('I\'m clicked!')
 //   let myAudio = document.getElementById("audio");
